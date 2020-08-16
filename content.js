a39c909374a5d0a4049999bfd6ebe2b8da9f4925
Jurie_OV <jurie.spies@openvantage.co.za>
2020-08-16 14:17:44 +0000

    added Xero invoice icon

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 // Prompts User otherwise recall from Local storage
 
 // Clear Local Storage
-// localStorage.clear()
+localStorage.clear()
 const nameStart = localStorage.getItem('nameStart') || prompt("What is your name ?").charAt(0).toUpperCase()
 const surnameStart = localStorage.getItem('surnameStart') || prompt("What is your surname ?").charAt(0).toUpperCase()
 
@@ -25,7 +25,7 @@ const getMonthName = {
     '11': 'December',
 }
 const currentMonth = getMonthName[new Date().getMonth()]
-// form ID's
+    // form ID's
 const receiptFrom = d.getElementsByClassName("x-form-text x-form-field autocompleter x-form-focus")[0]
 const date = d.getElementsByClassName("x-form-trigger x-form-date-trigger")[0]
 const reference = d.getElementsByClassName("field")[4]
@@ -36,14 +36,14 @@ const account = d.getElementsByClassName("dd")[1].children[0]
 const taxRate = d.getElementsByClassName("tax")[2]
 const amountZar = d.getElementsByClassName("field totalLineItem")[0]
 const bottomTotal = d.getElementById("invoiceTotal")
-//Values to be filled in
+    //Values to be filled in
 receiptFrom.value = "rain" //Receipt from
-date.click()//Date
-reference.value = `OV01 - Rain - ${localStorage.getItem('nameStart')}${localStorage.getItem('surnameStart')} - ${currentMonth} ${currentYear}`//ref
-topTotal.value = '500.00'//Total
+date.click() //Date
+reference.value = `OV01 - Rain - ${localStorage.getItem('nameStart')}${localStorage.getItem('surnameStart')} - ${currentMonth} ${currentYear}` //ref
+topTotal.value = '500.00' //Total
 description.value = `OV01 - Rain - ${localStorage.getItem('nameStart')}${localStorage.getItem('surnameStart')} - ${currentMonth} ${currentYear}` //Description
-account.value = "405 - Cell phone"//Account
-unitPrice.value = "500.00"//Unit Price
-taxRate.value = "Tax Exempt (0%)"//Tax Rate
-amountZar.value = "500.00"//Amount ZAR
-bottomTotal.value = "500.00"//Total
+account.value = "405 - Cell phone" //Account
+unitPrice.value = "500.00" //Unit Price
+taxRate.value = "Tax Exempt (0%)" //Tax Rate
+amountZar.value = "500.00" //Amount ZAR
+bottomTotal.value = "500.00" //Total
